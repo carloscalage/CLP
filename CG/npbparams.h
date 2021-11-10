5cc9bb773973a1fb21968fd3236ccd8fded005a2
Carlos Calage <calage.carlos@gmail.com>
2021-11-10 03:36:12 -0300

    first commit

---
+++ CG/npbparams.h
@@ -0,0 +1,21 @@
+/* CLASS = W */
+/*
+c  This file is generated automatically by the setparams utility.
+c  It sets the number of processors and the class of the NPB
+c  in this directory. Do not modify it by hand.
+*/
+#define	NA	7000
+#define	NONZER	8
+#define	NITER	15
+#define	SHIFT	12.0
+#define	RCOND	1.0e-1
+#define	CONVERTDOUBLE	FALSE
+#define COMPILETIME "10 Nov 2021"
+#define NPBVERSION "3.0 structured"
+#define CS1 "gcc"
+#define CS2 "gcc"
+#define CS3 "-lm"
+#define CS4 "-I../common"
+#define CS5 "-O3"
+#define CS6 "(none)"
+#define CS7 "randdp"
---
+++ IS/npbparams.h
@@ -0,0 +1,14 @@
+#define CLASS 'S'
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   */
+   
+#define COMPILETIME "10 Nov 2021"
+#define NPBVERSION "3.0 structured"
+#define CC "gcc"
+#define CFLAGS "-O3"
+#define CLINK "gcc"
+#define CLINKFLAGS "(none)"
+#define C_LIB "-lm"
+#define C_INC "-I../common"
